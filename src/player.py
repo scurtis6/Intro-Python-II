55cda7d6e890546ada58c92f1e06044b47910999
Sierra Curtis <sierracurtis91@gmail.com>
2020-05-06 19:41:05 -0400

    added str method to player and room

--- src/player.py
+++ src/player.py
@@ -4,3 +4,6 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+
+    def __str__(self):
+        return f'Player {self.name} is in room {self.current_room}'
