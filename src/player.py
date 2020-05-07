7ea2190fa3c30b0e890bbde01869cebab1ef9514
Sierra Curtis <sierracurtis91@gmail.com>
2020-05-07 16:26:13 -0400

    added item class and inventory

--- src/player.py
+++ src/player.py
@@ -3,9 +3,10 @@ from room import Room
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room: Room):
+    def __init__(self, name, current_room: Room, inventory=[]):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
 
     def __str__(self):
         return f'Player: {self.name}, Current Room: {self.current_room}'
